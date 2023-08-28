0fb29376034d23a8d31b0de8a8f723e6bc48d6a7
Adrian Ortiz <113073917+Adrianortiz-PR@users.noreply.github.com>
2023-08-27 23:52:20 -0400

    Add files via upload
    is remastered code is an improved version of the original code that's more organized, modular, and readable. It's suitable for sharing on GitHub or any other code repository.

--- Plot_the_ISS.py
+++ Plot_the_ISS.py
@@ -1,44 +1,48 @@
-def print_program_menu(): #this function was created to print the menu
+import urllib.request
+import json
+import webbrowser
+
+def print_program_menu():
     print("\n--------")
-    print( "Welcome to the Plot the ISS:")
+    print("Welcome to Plot the ISS:")
     print("Option one and two both redirect to a map.")
     print("1. Where is the ISS?")
     print("2. Verify the location")
     print("3. Exit")
 
+def get_iss_location():
+    URL = "http://api.open-notify.org/iss-now.json"
+    response = urllib.request.urlopen(URL)
+    data = json.loads(response.read())
+    return data["iss_position"]["latitude"], data["iss_position"]["longitude"]
 
+def open_map_with_coordinates(lat, lon):
+    open_map = "https://www.openstreetmap.org/?mlat=" + str(lat) + "&mlon=" + str(lon) + "#map=5/" + str(lat) + "/" + str(lon)
+    webbrowser.open_new(open_map)
+
+def open_nasa_tracking_map():
+    open_NASA = "https://spotthestation.nasa.gov/tracking_map.cfm"
+    webbrowser.open_new(open_NASA)
 
 def main():
     done = False
     while not done:
         print_program_menu()
-        userOption = input("Enter option: ")
-        if userOption.isdigit()== False or int(userOption) <=0 or int(userOption)>3:
-          print()
-          print("Option invalid")
-        else:# This Verify that a number was input
-         numericOption = int(userOption)
-         if numericOption ==1:
-            import urllib.request
-            import json
-            import webbrowser
-            URL= "http://api.open-notify.org/iss-now.json"
-            answer=urllib.request.urlopen(URL)
-            obj = json.loads(answer.read())
-            lat=obj["iss_position"]["latitude"]
-            lon=obj["iss_position"]["longitude"]
-            open_map="https://www.openstreetmap.org/?mlat=" + str(lat) + "&mlon=" + str(lon)+"#map=5/" + str(lat)+"/" + str(lon)
-            webbrowser.open_new(open_map)
-         if numericOption ==2:
-            import urllib.request
-            import webbrowser
-            open_NASA="https://spotthestation.nasa.gov/tracking_map.cfm"
-            webbrowser.open_new(open_NASA)
-
-         elif numericOption == 3:
-           print("Thanks for using 'Plot the ISS!")
-           done=True
+        user_option = input("Enter option: ")
+        
+        if user_option.isdigit() and 1 <= int(user_option) <= 3:
+            numeric_option = int(user_option)
+            
+            if numeric_option == 1:
+                lat, lon = get_iss_location()
+                open_map_with_coordinates(lat, lon)
+            elif numeric_option == 2:
+                open_nasa_tracking_map()
+            else:
+                print("Thanks for using 'Plot the ISS'!")
+                done = True
+        else:
+            print("Option invalid")
 
 if __name__ == "__main__":
     main()
-
